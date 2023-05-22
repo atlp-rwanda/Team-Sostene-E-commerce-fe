95ae52aa141cb50acbadc42aa5283298a27b4ce8
mirindi19 <mirindisaidi19@gmail.com>
2023-05-22 21:25:23 +0200

    ch(sass): Setup SASS processing
    - Install and setup sass pre-processing on the application to enable the use of both .scss and .css
    
    [Delivers #185172079]

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
